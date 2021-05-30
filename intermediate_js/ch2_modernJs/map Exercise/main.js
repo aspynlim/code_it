adf06529e4dddd42a28cfd073595f4da9e63f658
JenLim <cmdlhz@gmail.com>
2021-05-30 16:27:27 +0900

    map Exercise : AFTER

--- intermediate_js/ch2_modernJs/map Exercise/main.js
+++ intermediate_js/ch2_modernJs/map Exercise/main.js
@@ -1,7 +1,7 @@
 const quiz = ['YUMMY', 'COUNT', 'ABUSE', 'SOUND', 'SWING'];
 
 // 여기에 코드를 작성해 주세요.
-const answer = 
+const answer = quiz.map((word, i) => word[i]);
 
 // 결과 : "YOUNG"
-console.log(answer);
+console.log(answer);  // [ 'Y', 'O', 'U', 'N', 'G' ]
