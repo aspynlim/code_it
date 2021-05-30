97d8f2416e1d858fb99ff6143e4bb2bb94ff3bd3
JenLim <cmdlhz@gmail.com>
2021-05-30 21:50:49 +0900

    some & every Exercise : AFTER

--- intermediate_js/ch2_modernJs/Exercise_some_and_every/main.js
+++ intermediate_js/ch2_modernJs/Exercise_some_and_every/main.js
@@ -8,10 +8,29 @@ const spait = [
 ];
 
 function checkSpy(team) {
+  check = team.members.every((el) => el === '스파이');
   
+  if(check) {
+    message = `팀 ${team.codeName} 에는 이중 스파이가 없습니다.`;
+  } else {
+    message = `[주의!] 팀 ${team.codeName} 에 이중 스파이가 있습니다!`;
+  }
 
 	console.log(message);
-}
+};
+
+/* Destructuring 사용 + Tenary Operator(조건 연산자) */
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operator
+
+// function checkSpy(team) {
+//   const { codeName, members } = team;
+//   const result = members.every((member) => member === '스파이');
+//   const message = result
+//     ? `팀 ${codeName} 에는 이중 스파이가 없습니다.`
+//     : `[주의!] 팀 ${codeName} 에 이중 스파이가 있습니다!`;
+
+//   console.log(message);
+// }
 
 // 테스트 코드
 spait.forEach((team) => checkSpy(team));
