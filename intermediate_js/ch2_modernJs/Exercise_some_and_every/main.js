d8b8117b3cfb43d06e676ddd1bc11280fad60b73
JenLim <cmdlhz@gmail.com>
2021-05-30 21:41:36 +0900

    some & every Exercise : BEFORE

---
+++ intermediate_js/ch2_modernJs/Exercise_some_and_every/main.js
@@ -0,0 +1,17 @@
+const spait = [
+  { codeName: 'ApplePie', members: ['스파이', '스파이', '스파이', '스파이', '스파이'] },
+  { codeName: 'BigBoss', members: ['스파이', '스파이', '스과이', '스파이', '스파이'] },
+  { codeName: 'CEO', members: ['스파이', '스파이', '스파이', '습하이', '스파이'] },
+  { codeName: 'DeathNote', members: ['스파이', '스파이', '스파이', '스파이', '스파이'] },
+  { codeName: 'EarlyBird', members: ['스파이', '스마이', '스파이', '스파이', '스파이'] },
+  { codeName: 'Faker', members: ['스파이', '스파이', '스파이', '스파이', '스파이'] },
+];
+
+function checkSpy(team) {
+  
+
+	console.log(message);
+}
+
+// 테스트 코드
+spait.forEach((team) => checkSpy(team));
