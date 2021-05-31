f27cd29554dce321040993063219fe072a2b48ea
aspynlim <aspyn.lim@gmail.com>
2021-05-31 11:58:22 +0900

    reduce Exercise: BEFORE

---
+++ intermediate_js/ch2_modernJs/Exercise_reduce/main.js
@@ -0,0 +1,13 @@
+const data = [ 
+  { company: 'Naber', month: 3 },
+	{ company: 'Amajohn', month: 12 },
+	{ company: 'Coogle', month: 10 },
+  { company: 'Ittel', month: 6 },
+  { company: 'Sasung', month: 4 },
+  { company: 'CaCao', month: 3 },
+	{ company: 'Microhard', month: 17 },
+];
+
+const totalCareer = 
+
+console.log(`상원이의 경력은 총 ${totalCareer}개월입니다.`);
