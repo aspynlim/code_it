f722aa8af3fea50e758a2327aa68c221fcdcaebe
aspynlim <aspyn.lim@gmail.com>
2021-06-02 18:41:49 +0900

    Four Pillars: Encapsulation (Exercise : AFTER - Closure)

---
+++ intermediate_js/ch3_object-oriented_JS/Four_pillars/2_encapsulation/exercise_closure.js
@@ -0,0 +1,25 @@
+function BankAccount(name, balance) {
+  let _balance = balance;
+  const user = {
+    name,
+
+    get balance() {
+      return _balance;
+    },
+  
+    set balance(money){
+      if(money >= 0){
+        _balance = money;
+      } else {
+        console.log('You cannnot set negative number for balance');
+      }
+    }
+  };
+  return user;
+}
+
+const account1 = new BankAccount('Michael', 10000);
+account1.balance = 20000;
+console.log(account1.balance);  // 20000
+console.log(account1._balance); // undefined
+account1.balance = -5000;       // You cannnot set negative number for balance
