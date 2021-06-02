6429068cb47a3e2fd23d4ffbf619be340f47d29a
aspynlim <aspyn.lim@gmail.com>
2021-06-02 18:08:13 +0900

    Four Pillars: Encapsulation (Exercise : BEFORE)

---
+++ intermediate_js/ch3_object-oriented_JS/Four_pillars/2_encapsulation/exercise.js
@@ -0,0 +1,36 @@
+class BankAccount {
+  constructor(name, money) {
+    this.holder = name;
+    this.balance = money;
+  }
+
+  get balance() {
+    return this._balance;
+  }
+
+  deposit(money) {
+    this.balance += money;
+  }
+
+  withdraw(money) {
+    if (this.balance - money < 0) {
+      console.log('Insufficient balance');
+    } else {
+      this.balance -= money;
+    }
+  }
+
+  transfer(money, anotherAccount) {
+    const account = anotherAccount;
+    if (this.balance - money < 0) {
+      console.log('Insufficient balance');
+    } else {
+      this.balance -= money;
+      account.balance += money;
+    }
+  }
+}
+
+const account1 = new BankAccount('Michael', 10000);
+account1.balance = 20000;
+account1.balance = -5000;
