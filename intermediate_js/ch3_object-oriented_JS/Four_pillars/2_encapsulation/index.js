133ebe848da69dd430b88f809a205f9119b7634f
aspynlim <aspyn.lim@gmail.com>
2021-06-02 16:07:12 +0900

    Four Pillars: Encapsulation (BEFORE) 삭제된 부분 복구

--- intermediate_js/ch3_object-oriented_JS/Four_pillars/2_encapsulation/index.js
+++ intermediate_js/ch3_object-oriented_JS/Four_pillars/2_encapsulation/index.js
@@ -3,22 +3,16 @@ class User {
     this.email = email;
     this.birthDate = birthDate;
   }
-
   buy(item){
     console.log(`${this.email} buys a ${item.name}.`);
   };
 };
 
-const item = {
-  name: 'sweater',
-  price: 30000
-};
+const user1 = new User('chris123@google.com', '1992-03-21');
 
-const user1 = new User('smith@gmail.com', '2000-01-10');
-console.log(user1.email);  // smith@gmail.com
+console.log(user1.email); // chris123@google.com
 
 user1.email = 'Chris Robert';
 
 console.log(user1);
-console.log(user1.email);  // Chris Robert
-// 이메일 형식이 아닌데도 수정이 됨
+console.log(user1.email); // Chris Robert
