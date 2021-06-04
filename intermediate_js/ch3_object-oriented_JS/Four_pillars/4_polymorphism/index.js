8806ae634767f711bbb6298382ea8e16067a9249
aspynlim <aspyn.lim@gmail.com>
2021-06-04 18:14:52 +0900

    Four Pillars: Polymorphism (AFTER)

--- intermediate_js/ch3_object-oriented_JS/Four_pillars/4_polymorphism/index.js
+++ intermediate_js/ch3_object-oriented_JS/Four_pillars/4_polymorphism/index.js
@@ -15,6 +15,10 @@ class PremiumUser extends User {
     this.level = level;
   }
 
+  buy(item){
+    console.log(`${this.email} buys ${item.name} with a 5% discount.`);
+  }
+
   streamMusicForFree(){
     console.log(`Free music streaming for ${this.email}.`)
   }
@@ -25,4 +29,14 @@ const item = {
   price: 30000
 }
 
+const user1 = new User('jen@gmail.com', '1999-02-09');
 const pUser1 = new PremiumUser('test@gmail.com', '2000-10-02', 3);
+
+const users = [user1, pUser1];
+
+users.forEach((user) => {
+  user.buy(item);
+});
+
+// jen@gmail.com buys sweater.
+// test@gmail.com buys sweater with a 5% discount.
