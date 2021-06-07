eb88e3b1d44b2bed0ea3d5d4009d5726db18160e
aspynlim <aspyn.lim@gmail.com>
2021-06-07 16:09:07 +0900

    Web API : JSON (Exercise : AFTER)

--- intermediate_js/ch4_web_development/2_Web_API/JSON.js
+++ intermediate_js/ch4_web_development/2_Web_API/JSON.js
@@ -8,7 +8,7 @@
 fetch('https://learn.codeit.kr/api/topics')
   .then((response) => response.text())
   .then((result) => {
-    const topics = // 여기에 코드를 작성하세요.
+    const topics = JSON.parse(result);
     const beginnerLevelTopics = topics.filter((topic) => topic.difficulty === '초급');
     console.log(beginnerLevelTopics);
   });
