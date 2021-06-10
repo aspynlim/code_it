44aefbca93f7476ff43e5c6084cddbca850b46db
aspynlim <aspyn.lim@gmail.com>
2021-06-10 19:01:31 +0900

    Promise Object: <finally> method (Exercise: AFTER)

--- intermediate_js/ch4_web_development/3_Promise/finally.js
+++ intermediate_js/ch4_web_development/3_Promise/finally.js
@@ -2,7 +2,7 @@ let isLoading = true;
 
 /* ..다른 코드들 */
 
-// const url = 'https://jsonplaceholder.typicode.com/users';
+const url = 'https://jsonplaceholder.typicode.com/users';
 // const url = 'https://www.google.com';
 
 fetch(url)
@@ -14,12 +14,14 @@ fetch(url)
     throw new Error('response is not json data');
   })
   .then((result) => {
-  // 리스폰스 처리
     console.log(result);
   })
   .catch((error) => {
-  // 에러 처리
     console.log(error);
+  })
+  .finally(() => {
+    isLoading = false;
+    console.log(isLoading);
   });
 
 /* ..다른 코드들 */
