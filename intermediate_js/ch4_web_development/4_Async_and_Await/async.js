e7f97c56352e7dccac29622f0d96a67f1856aa19
JenLim <cmdlhz@gmail.com>
2021-06-12 20:53:06 +0900

    Async/Await: Promise Chaining (Exercise : AFTER)

--- intermediate_js/ch4_web_development/4_Async_and_Await/async.js
+++ intermediate_js/ch4_web_development/4_Async_and_Await/async.js
@@ -1,20 +1,32 @@
-fetch("https://jsonplaceholder.typicode.com/users")
-  .then((response) => response.json())
-  .then((users) => {
-    const lastUser = users[users.length - 1];
-    return lastUser.id;
-  })
-  .then((id) => fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`))
-  .then((response) => response.json())
-  .then((posts) => {
-    const lastPost = posts[posts.length - 1];
-    console.log(lastPost);
-  });
+// fetch("https://jsonplaceholder.typicode.com/users")
+//   .then((response) => response.json())
+//   .then((users) => {
+//     const lastUser = users[users.length - 1];
+//     return lastUser.id;
+//   })
+//   .then((id) => fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`))
+//   .then((response) => response.json())
+//   .then((posts) => {
+//     const lastPost = posts[posts.length - 1];
+//     console.log(lastPost);
+//   });
 
-/* async function getTheLastPostOfTheLastUser() {
+async function getTheLastPostOfTheLastUser() {
+  const usersJSON = await fetch("https://jsonplaceholder.typicode.com/users");
 
+  const users = await usersJSON.json();
+  const lastUser = users[users.length - 1];
+
+  const id = await lastUser.id;
+  // const { id } = lastUser;
+
+  const postsJSON = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`);
+
+  const posts = await postsJSON.json();
+  const lastPost = posts[posts.length - 1];
+  return lastPost;
 }
 
 getTheLastPostOfTheLastUser().then((lastPost) => {
   console.log(lastPost);
-}); */
+});
